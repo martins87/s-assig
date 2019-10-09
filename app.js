94385c2374effed4c203d9c700c0270d1c3b4b71
Daniel Martins <martinsda@gmail.com>
2019-10-09 09:34:38 -0300

    Add create user, adding to mongodb database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const User = require('./models/user')
 
 const dbUrl = 'mongodb://localhost:27017/salamantex'
@@ -22,8 +23,49 @@ var app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.render('index', { title: 'Salamantex' })
+})
+
+app.get('/create-user', (req, res) => {
+    res.render('create-user', { title: 'Create User' })
+})
+
+app.post('/create-user', (req, res) => {
+    var data = req.body
+    console.log("User name: ", data.name)
+    console.log("description: ", data.description)
+    console.log("email: ", data.email)
+    console.log("bitcoinWalletId: ", data.bitcoinWalletId)
+    console.log("ethereumWalletId: ", data.ethereumWalletId)
+    console.log("maxAmountAllowed: ", data.maxAmountAllowed)
+
+    User.create({
+        id: "101",
+        name: data.name,
+        description: data.description,
+        email: data.email,
+        bitcoinWalletId: data.bitcoinWalletId,
+        bitcoinBalance: "0",
+        ethereumWalletId: data.ethereumWalletId,
+        etherBalance: "0",
+        maxAmountAllowed: data.ethereumWalletId,
+    }, (err, data) => {
+        if(err) {
+            console.log('There was a problem adding a document to users collection')
+            console.log(err)
+        } else {
+            console.log('Data successfully added to database:')
+            console.log(data)
+        }
+
+    })
+
+    res.render('create-user', { title: 'Create User' })
 })
 
 const port = 3000
--- models/transaction.js
+++ models/transaction.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 var transactionSchema = new Schema({
-
+    id: String,
+    currencyAmount: String,
+    currencyType: String,
+    sourceUserId: String,
+    targetUserId: String,
+    timestampCreated: String,
+    timestampProcessed: String,
+    state: String,
 })
 
 const Transaction = mongoose.model('transactions', transactionSchema)
